27a0a6e9dfb07d7be3826e1e551382ad4f4d609e
= <=harrishkumar100@gmail.com>
2022-09-18 16:14:12 +0530

    Accomodation form layout over

--- src/components/pages/Accomodation.js
+++ src/components/pages/Accomodation.js
@@ -2,81 +2,151 @@ import React, { useState } from 'react';
 
 function Accomodation() {
 
-    const [sel,setsel] = useState('single');
-    const [room,setr] = useState(0);
-    const [total,settotal] = useState([]);
-    const [roomcost,setco] = useState(0);
-    const [tval,sett] = useState(0);
-
-    function add(e){
-        if(room > 0){
-            settotal([sel + '-' + room, ...total]);
-        }
-    }
-
-    function filter(s){
-        var t1 = [];
-        var done = 1;
-        for(var i of total){
-            if(i == s.s && done){
-                done = 0;
-            }
-            else{
-                t1.push(i);
-            }
-        }
-        settotal(t1);
-    }
-
-    function froomcost(r){
-        var t;
-        if(sel == 'single'){
-            t = (r)*2800;
-        }
-        else if(sel == 'double'){
-            t = r*3500;
-        }
-        else{
-            t = r*1750;
-        }
-        setco(t);
-    }
-
+    const [fam,setfam] = useState("single");
+    const [rreq,setrreq] = useState("notRequired");
+    const [roomt,setroomt] = useState(null);
+    const [cin,setcin] = useState(null);
+    const [cout,setcout] = useState(null);
+    const [hotel,sethotel] = useState(null);
     var key = 0;
     return (
         <>
          <h1>Accomodation</h1>
-            <label>Choose Accomodation : 
-                <select name='acc' onChange={e => {
-                    setsel(e.target.value);
-                    sett(0);
-                    setco(0);
-                    }}>
-                    <option value="single">Single</option>
-                    <option value="double">Double</option>
-                    <option value="tshare">T-share</option>
+         <label>Participation Type 
+            <select onChange={(e) => setfam(e.target.value)}>
+                <option value="single" >single</option>
+                <option value="withFamily" >withFamily</option>
+            </select>
+            </label>
+            <br />
+            <br />
+            {fam == 'withFamily' && <div className='fdetails'>
+                    <table>
+                    <tbody>
+                        <tr>
+                        <td><label>Alumni count 
+                        
+                    </label></td>
+                    <td><input type="number" min = "0"/></td>
+                        </tr>
+                        <tr>
+                        <td>
+                    <label>Spouse count 
+                        
+                    </label></td>
+                    <td><input type="number" min = "0"/></td>
+                        </tr>
+                        <tr>
+                        <td>
+                    <label>Family members count 
+                        
+                    </label></td>
+                    <td><input type="number" min = "0"/></td>
+                        </tr>
+                        <tr>
+                        <td>
+                        <label>Grand Kids count 
+                    </label></td>
+                    <td> <input type="number" min = "0"/>
+</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                    
+                    
+                    
+                    
+                </div>}
+                <br />
+            <br />
 
-                </select>
+            <label>Hotel Room 
+            <select onChange={(e) => setrreq(e.target.value)}>
+                <option value="notRequired" >notRequired</option>
+                <option value="required" >required</option>
+            </select>
             </label>
+            <br />
+            <br />
+            {rreq == 'required' && <div className='rdetails'>
+            <table>
+            <tbody>
+            <tr>
+            <td><label>Type of room </label></td>
+            <td><select onChange={(e) => setroomt(e.target.value)}>
+                <option value="singleOccupancy" >singleOccupancy</option>
+                <option value="doubleOccupancy" >doubleOccupancy</option>
+                <option value="twinShare" >twinShare</option>
+
+            </select></td>
+            </tr>
+            <tr>
+            <td><label>CheckIn</label></td>
             
-            <label>No of Rooms : 
-            <input value={tval} type="number" onChange = {e => {
-                setr(e.target.value);
-                froomcost(e.target.value);
-                sett(tval + 1);
-                }} min = "0"/>
-            </label>
-            <p>Cost : {roomcost}</p>
-            <button onClick={add}>Submit</button>
-            {total.map(s => {
-            key += 1;
-            if(s != ''){
-            return(<div key={key}>
-                <p >{s}</p>
-                <button onClick={() => filter({s})}>Remove</button>
-            </div>)}
-            }
-            )}
+            <td><input type="date"  onChange={(e) => setcin(e.target.value)}/></td>
+            </tr>
+            <tr>
+            <td><label>CheckOut</label></td>
+            <td><input type="date"  onChange={(e) => setcout(e.target.value)}/></td>
+            </tr>
+            <tr>
+            <td><label>Hotel</label></td>
+            <td><select onChange={(e) => sethotel(e.target.value)}>
+                <option >Select</option>
+                <option value="breezeResidency">breezeResidency</option>
+                <option value="hotelTamilNadu">hotelTamilNadu</option>
+
+            </select></td>
+            </tr>
+            </tbody>
+            </table>
+            
+            
+            </div>}
+            <br />
+            <br />
+            {hotel == "breezeResidency" && <div className='roomdetails'>
+                    <table>
+                        <tbody>
+                        <tr>
+                        <td><label>RoomType</label></td>
+                            <td><select onChange={(e) => console.log(e.target.value)}>
+                                <option >Select</option>
+                                <option value="standard">standard</option>
+                                <option value="executive">executive</option>
+                                <option value="deluxe">deluxe</option>
+                                <option value="luxurySuite">luxurySuite</option>
+                                <option value="grandSuite">grandSuite</option>
+
+
+                            </select></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>}
+
+            {hotel == "hotelTamilNadu" && <div className='roomdetails'>
+            <table>
+            <tbody>
+                        <tr>
+                        <td><label>RoomType</label></td>
+                            <td><select onChange={(e) => console.log(e.target.value)}>
+                                <option >Select</option>
+                                <option value="standard">standard</option>
+                                <option value="familyRoom">familyRoom</option>
+                                <option value="deluxe">deluxe</option>
+                                <option value="suite">suite</option>
+                                <option value="additionalMember">additionalMember</option>
+
+
+                            </select></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>}
+
+            
+            <button onClick={(e) => console.log(e)}>Submit</button>
 
         </>
         
